9affaa9755092a8232d260354422bb414e56ef7b
Joao Barbon <joao.pradobarbon@gmail.com>
2023-07-14 01:30:30 -0300

    First commit

---
+++ bluetooth_config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo systemctl start bluetooth # to start it for the session will stay disabled after reboot.
+sudo systemctl enable bluetooth # to enable per default, will run after every boot.
+
+sudo pacman -S --needed bluez bluez-utils
+sudo pacman -S blueman
---
+++ packages_config/discord/discord_config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Create file backup
+cp $HOME/.config/discord/settings.json $HOME/.config/discord/settings_backup.json
+cp ./settings.json $HOME/.config/discord/
